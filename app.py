d6a30d2ee1f3008f89baa0571cf64468d7354399
Mark Rattle <111889289+energeist@users.noreply.github.com>
2022-10-25 15:12:45 -0400

    Added missing docstring
    Also added for loop in comment for say_n_times function.

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def generate_madlib(adjective, noun):
 
 @app.route('/multiply/<number1>/<number2>')
 def multiply_numbers(number1, number2):
-    """takes in 2 numbers, multiplies them, and displays the results."""
+    """Takes in 2 numbers, multiplies them, and displays the results."""
     if number1.isdigit() and number2.isdigit():
         result = f'{number1} times {number2} is {int(number1) * int(number2)}'
     else:
@@ -38,14 +38,23 @@ def multiply_numbers(number1, number2):
 
 @app.route('/sayntimes/<word>/<n>')
 def say_n_times(word, n):
-    if word.isalpha() and str(n).isdigit():
+    """Takes word as a string and prints it n times with a space between each instance of word"""
+    output = ""
+    if word.isalpha() and n.isdigit():
         output =  (word + " ") * (int(n)-1) + word
+        
+        # The problem statement asks for a for loop, but I think multiply a string is more efficient in this case. Working for loop syntax is included below for reference.
+  
+        # for i in range(int(n)-1):
+        #     output += (word + " ")
+        # output += word
     else:
         output = "Invalid input. Please try again by entering a word and a number!"
     return output
 
 @app.route('/dicegame')
 def play_dice_game():
+    """Randomly generates a number from 1 to 6 as a D6 dice roll.  Result of 3 or less is a loss, result of 4 or more is a win. """
     dice_roll = random.randint(1,6)
     if dice_roll > 3:
         output = f"You rolled a {dice_roll}. You won!"
@@ -56,4 +65,3 @@ def play_dice_game():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
